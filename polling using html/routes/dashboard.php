52b7ce4d63556bd034b639dbeede075bf5820cce
kunalsharma2020 <132328310+kunalsharma2020@users.noreply.github.com>
2023-06-29 23:55:10 +0530

    Add files via upload

---
+++ polling using html/routes/dashboard.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>online voting system - dashboard</title>
+    </head>
+    <body>
+        <button>back</button>
+        <button>logout</button>
+        <h1>online voting system</h1>
+        <hr>
+        <div id ="Profile"></div>
+        <div id ="Group"></div>
+    </body>
+</html>
